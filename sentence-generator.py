f8097d68c3c404b25fe3e522f734a35da3341d19
Harry Schwartz <hrs@ginkgobioworks.com>
2013-06-23 21:41:56 -0400

    use more standard paren spacing

--- sentence-generator.py
+++ sentence-generator.py
@@ -22,13 +22,13 @@ mapping = {}
 starts = []
 
 # We want to be able to compare words independent of their capitalization.
-def fixCaps (word):
+def fixCaps(word):
 	# Ex: "FOO" -> "foo"
-    if word.isupper () and word != "I":
-        word = word.lower ()
+    if word.isupper() and word != "I":
+        word = word.lower()
 	# Ex: "LaTeX" => "Latex"
-    elif word [0].isupper ():
-        word = word.lower().capitalize ()
+    elif word [0].isupper():
+        word = word.lower().capitalize()
 	# Ex: "wOOt" -> "woot"
     else:
         word = word.lower()
@@ -37,15 +37,15 @@ def fixCaps (word):
 # Tuples can be hashed; lists can't.  We need hashable values for dict keys.
 # This looks like a hack (and it is, a little) but in practice it doesn't
 # affect processing time too negatively.
-def toHash (lst):
-    return tuple (lst)
+def toHashKey(lst):
+    return tuple(lst)
 
 # Returns the contents of the file, split into a list of words and
 # (some) punctuation.
-def wordlist (filename):
-	f = open (filename, 'r')
-	wordlist = [fixCaps (w) for w in re.findall (r"[\w']+|[.,!?;]", f.read ())]
-	f.close ()
+def wordlist(filename):
+	f = open(filename, 'r')
+	wordlist = [fixCaps(w) for w in re.findall(r"[\w']+|[.,!?;]", f.read())]
+	f.close()
 	return wordlist
 
 # Self-explanatory -- adds "word" to the "tempMapping" dict under "history".
@@ -53,86 +53,85 @@ def wordlist (filename):
 # words.
 # Given history = ["the", "rain", "in"] and word = "Spain", we add "Spain" to
 # the entries for ["the", "rain", "in"], ["rain", "in"], and ["in"].
-def addItemToTempMapping (history, word):
+def addItemToTempMapping(history, word):
 	global tempMapping
-	while len (history) > 0:
-		first = toHash (history)
+	while len(history) > 0:
+		first = toHashKey(history)
 		if first in tempMapping:
-			if word in tempMapping [first]:
-				tempMapping [first][word] += 1.0
+			if word in tempMapping[first]:
+				tempMapping[first][word] += 1.0
 			else:
-				tempMapping [first][word] = 1.0
+				tempMapping[first][word] = 1.0
 		else:
-			tempMapping [first] = {}
-			tempMapping [first][word] = 1.0
-		history = history [1:]
+			tempMapping[first] = {}
+			tempMapping[first][word] = 1.0
+		history = history[1:]
 
 # Building and normalizing the mapping.
-def buildMapping (wordlist, markovLength):
+def buildMapping(wordlist, markovLength):
 	global tempMapping
-	starts.append (wordlist [0])
-	for i in range (1, len (wordlist) - 1):
+	starts.append(wordlist [0])
+	for i in range(1, len(wordlist) - 1):
 		if i <= markovLength:
-			history = wordlist [: i + 1]
+			history = wordlist[: i + 1]
 		else:
-			history = wordlist [i - markovLength + 1 : i + 1]
-		follow = wordlist [i + 1]
+			history = wordlist[i - markovLength + 1 : i + 1]
+		follow = wordlist[i + 1]
 		# if the last elt was a period, add the next word to the start list
-		if history [-1] == "." and follow not in ".,!?;":
-			starts.append (follow)
-		addItemToTempMapping (history, follow)
+		if history[-1] == "." and follow not in ".,!?;":
+			starts.append(follow)
+		addItemToTempMapping(history, follow)
 	# Normalize the values in tempMapping, put them into mapping
-	for first, followset in tempMapping.iteritems ():
-		total = sum (followset.values ())
+	for first, followset in tempMapping.iteritems():
+		total = sum(followset.values())
 		# Normalizing here:
-		mapping [first] = dict ([(k, v / total) for k, v in followset.iteritems ()])
+		mapping[first] = dict([(k, v / total) for k, v in followset.iteritems()])
 
 # Returns the next word in the sentence (chosen randomly),
 # given the previous ones.
-def next (prevList):
+def next(prevList):
 	sum = 0.0
 	retval = ""
-	index = random.random ()
+	index = random.random()
 	# Shorten prevList until it's in mapping
-	while toHash (prevList) not in mapping:
-		prevList.pop (0)
+	while toHashKey(prevList) not in mapping:
+		prevList.pop(0)
 	# Get a random word from the mapping, given prevList
-	for k, v in mapping [toHash (prevList)].iteritems ():
+	for k, v in mapping[toHashKey(prevList)].iteritems():
 		sum += v
 		if sum >= index and retval == "":
 			retval = k
 	return retval
 
-def genSentence (markovLength):
+def genSentence(markovLength):
 	# Start with a random "starting word"
-	curr = random.choice (starts)
-	sent = curr.capitalize ()
+	curr = random.choice(starts)
+	sent = curr.capitalize()
 	prevList = [curr]
 	# Keep adding words until we hit a period
 	while (curr not in "."):
-		curr = next (prevList)
-		prevList.append (curr)
+		curr = next(prevList)
+		prevList.append(curr)
 		# if the prevList has gotten too long, trim it
-		if len (prevList) > markovLength:
-			prevList.pop (0)
+		if len(prevList) > markovLength:
+			prevList.pop(0)
 		if (curr not in ".,!?;"):
 			sent += " " # Add spaces between words (but not punctuation)
 		sent += curr
 	return sent
 
-def main ():
-	if len (sys.argv) < 2:
-		sys.stderr.write ('Usage: ' + sys.argv [0] + ' text_source [chain_length=1]\n')
-		sys.exit (1)
+def main():
+	if len(sys.argv) < 2:
+		sys.stderr.write('Usage: ' + sys.argv [0] + ' text_source [chain_length=1]\n')
+		sys.exit(1)
 
-	filename = sys.argv [1]
+	filename = sys.argv[1]
 	markovLength = 1
 	if len (sys.argv) == 3:
-		markovLength = int (sys.argv [2])
+		markovLength = int(sys.argv [2])
 
-	buildMapping (wordlist (filename), markovLength)
-	print genSentence (markovLength)
+	buildMapping(wordlist(filename), markovLength)
+	print genSentence(markovLength)
 
 if __name__ == "__main__":
 	main()
-	
